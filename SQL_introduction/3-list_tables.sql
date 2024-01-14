44c515cfc782c16d6b15831c5bd1d864a33e2ef6
mesoud5 <mesoudtemam5@gmail.com>
2024-01-14 08:02:05 -0700

    b

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,10 @@
+-- we will create a database
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 -- we will use mysql database
-USE mysql;
+USE hbtn_test_db_0;
+-- we will create a table
+CREATE TABLE IF NOT EXISTS holbteron_0 (
+    id INT
+);
 -- we will see the tables in the database
 SHOW TABLE;
