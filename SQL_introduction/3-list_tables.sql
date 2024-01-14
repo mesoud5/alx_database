90dcecc75db3ae2eb251c1a64f28502528d98010
mesoud5 <mesoudtemam5@gmail.com>
2024-01-14 08:09:31 -0700

    TASK 3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,17 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 -- we will use mysql database
 USE hbtn_test_db_0;
--- we will create a table
-CREATE TABLE IF NOT EXISTS holbteron_0 (
-    id INT
-);
-CREATE TABLE IF NOT EXISTS holbteron_1 (
-    name VARCHAR(256)
-);
 
-CREATE TABLE IF NOT EXISTS holbteron_2 (
-    id INT,
-    name VARCHAR(256)
-);
 -- we will see the tables in the database
 SHOW TABLES;
