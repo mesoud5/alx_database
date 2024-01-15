322890dc4df88b43505f1c863a10d2c87056bb91
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 04:04:41 -0700

    task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,7 @@
 -- Create database and table
-DROP DATABASE IF EXISTS hbtn_test_db_5;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
-USE hbtn_test_db_5;
+DROP DATABASE IF EXISTS hbtn_test_db_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+USE hbtn_test_db_0;
 CREATE TABLE IF NOT EXISTS first_table (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(128),
@@ -9,8 +9,6 @@ CREATE TABLE IF NOT EXISTS first_table (
     created_at DATE,
     PRIMARY KEY (id)
 );
-
--- Query the information_schema.columns to get full description of the table
 SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'first_table';
