ed796b13c8dca8acb33937902bae6d2fd8ac73db
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 07:13:28 -0700

    task 8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,1 +1,19 @@
 -- COMMENT
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Create records
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
+SELECT COUNT(id) FROM first_table;
