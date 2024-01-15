8f3afb70c660c091a83bf461de907f8231650c09
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 05:25:30 -0700

    jhf

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -3,6 +3,10 @@ DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- using hbtn_test_db_0 database
 USE hbtn_0c_0;
-
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
 -- listing all rows in the table:fisrt_table
 SELECT * FROM first_table;
