167b6dabd6e4e262cf2cb848769565014fd22cd2
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 01:04:43 -0700

    task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -12,4 +12,4 @@ CREATE TABLE IF NOT EXISTS first_table (
 -- Insert 2 values and print count
 INSERT INTO first_table (id, name) VALUES (1, "First name");
 INSERT INTO first_table (id, name) VALUES (2, "Last name");
-
+-- SELECT COUNT(id) FROM first_table;
