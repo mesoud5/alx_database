62a647e003179ed196fa869f23169000988ee28c
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 01:39:07 -0700

    task 5

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -11,8 +11,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     name VARCHAR(256)
 );
 
--- Insert 2 values and print count
--- INSERT INTO first_table (id, name) VALUES (1, "First name");
--- INSERT INTO first_table (id, name) VALUES (2, "Last name");
-
--- SELECT COUNT(id) FROM first_table;
+ -- Insert 2 values and print count
+ INSERT INTO first_table (id, name) VALUES (1, "mesoud");
+ INSERT INTO first_table (id, name) VALUES (2, "temam");
+ SELECT COUNT(id) FROM first_table;
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Using hbtn_test_db_0 database
+USE hbtn_test_db_4;
+
+-- Showing all rows of TABLE: first_table in hbtn_test_db_0 database
+SELECT * FROM first_table;
