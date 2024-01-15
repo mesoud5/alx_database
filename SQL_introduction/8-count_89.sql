cd6a0f7b4cca5733c91f5189346a2a91c6c9ad82
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 07:14:23 -0700

    task 8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS first_table (
 INSERT INTO first_table (id, name) VALUES (1, "A");
 INSERT INTO first_table (id, name) VALUES (89, "B");
 INSERT INTO first_table (id, name) VALUES (2, "C");
-INSERT INTO first_table (id, name) VALUES (89, "D");
-INSERT INTO first_table (id, name) VALUES (89, "E");
-INSERT INTO first_table (id, name) VALUES (3, "F");
+-- INSERT INTO first_table (id, name) VALUES (89, "D");
+-- INSERT INTO first_table (id, name) VALUES (89, "E");
+-- INSERT INTO first_table (id, name) VALUES (3, "F");
 SELECT COUNT(id) FROM first_table;
