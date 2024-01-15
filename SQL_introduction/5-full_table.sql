45300669d8c5fe34eccc1f5ec8b315eef276eaab
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 01:56:10 -0700

    alne

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,16 @@
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
 -- Using hbtn_test_db_0 database
-USE hbtn_test_db_4;
+-- USE hbtn_test_db_4;
 
 -- Showing all rows of TABLE: first_table in hbtn_test_db_0 database
-SHOW CREATE TABLE first_table;
+SELECT * FROM first_table;
