b459bfdebf9439761f837210ea8b2e31051df57d
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 07:04:20 -0700

    task 7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,18 +1,15 @@
 -- Task: Create database, table, insert record
 
 -- Create database
-DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Switch to the specified database
-USE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_test_db_7;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_7;
+USE hbtn_test_db_7;
 
 -- Create table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
-
 -- Insert a record into first_table
 INSERT INTO first_table (id, name) VALUES (89, 'holberton school');
-SELECT * FROM first_table;
+SELECT id, name FROM first_table ORDER BY id;
