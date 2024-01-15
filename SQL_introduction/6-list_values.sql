c6b7613c5e108c1703d11ab0e441aa3c331966f4
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 05:36:30 -0700

    task 6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -8,10 +8,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
--- Insert 3 records
-INSERT INTO first_table (id, name) VALUES (1, "First name");
-INSERT INTO first_table (id, name) VALUES (2, "Last name");
-INSERT INTO first_table (id, name) VALUES (3, "name");
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
