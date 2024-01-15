c4bc99b0fb69dd68d8bc070689396fa24226ba33
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 11:48:01 -0700

    GHF

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -8,4 +8,51 @@ CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
+
+-- Create records
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
+
+-- Count records with id = 89
 SELECT COUNT(*) FROM first_table WHERE id = 89;
+
+-- Select 5 records with id = 89
+UNION ALL
+
+SELECT *
+FROM first_table
+WHERE id = 89
+LIMIT 5;
+-- COMMENT
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Create records
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
+
+-- Count records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
+-- Select 5 records with id = 89
+UNION ALL
+
+SELECT *
+FROM first_table
+WHERE id = 89
+LIMIT 5;
