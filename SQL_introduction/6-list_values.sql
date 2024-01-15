2cd4239ae552df97b4c73720bda7a07bf4dcb5e2
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 05:22:33 -0700

    task 6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,8 +1,8 @@
 -- Create database 
-DROP DATABASE IF EXISTS hbtn_test_db_0;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- using hbtn_test_db_0 database
-USE hbtn_test_db_0;
+USE hbtn_0c_0;
 
 -- listing all rows in the table:fisrt_table
 SELECT * FROM first_table;
