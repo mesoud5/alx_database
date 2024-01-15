35cde78090fb043eb2909e0722d7ff541544220c
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 02:31:19 -0700

    task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,10 +9,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     created_at DATE,
     PRIMARY KEY (id)
 );
--- Switch to the desired database
-USE hbtn_0c_0;
-
 -- Query the information_schema.columns to get full description of the table
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'first_table';
