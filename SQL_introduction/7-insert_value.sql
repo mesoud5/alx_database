9e401cbcdde83e6992eee57ccf58f60301a81184
mesoud5 <mesoudtemam5@gmail.com>
2024-01-15 05:48:14 -0700

    COMMIT

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,14 +1,11 @@
 -- Create database 
 DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- using hbtn_test_db_0 database
+-- using hbtn_0C_0 database
 USE hbtn_0c_0;
 -- Create table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
-
-
--- listing all rows in the table:fisrt_table
-SELECT * FROM first_table;
+INSERT INTO first_table(id, name) VALUES (89, "holberton school")
