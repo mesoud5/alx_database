1eb3096bc1080c4ca00ef5b60bccdd7826fc2f2b
mesoud5 <mesoudtemam5@gmail.com>
2024-01-18 01:04:38 -0700

    TASK -0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,2 @@
--- MORE SQL QUERIES
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-SHOW DATABASES;
