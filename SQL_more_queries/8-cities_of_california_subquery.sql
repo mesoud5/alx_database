d87c68a6a0ce75ce58a8950e834d651d4ab3fdd2
mesoud5 <mesoudtemam5@gmail.com>
2024-01-18 11:30:14 -0700

    TASK 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,35 +1,7 @@
--- CITIES IN CALIFORNIA
--- Using hbtn_0d_usa database
--- New database hbtn_test_db_8
-DROP DATABASE IF EXISTS hbtn_test_db_8;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
-USE hbtn_test_db_8;
-
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-
-INSERT INTO states (id, name) VALUES (1, "California");
-INSERT INTO states (id, name) VALUES (2, "Arizona");
-INSERT INTO states (id, name) VALUES (3, "Nevada");
-
-INSERT INTO cities (id, state_id, name) VALUES (1, 1, "San Francisco");
-INSERT INTO cities (id, state_id, name) VALUES (2, 1, "San Diego");
-INSERT INTO cities (id, state_id, name) VALUES (3, 1, "San Jose");
-INSERT INTO cities (id, state_id, name) VALUES (10, 2, "Page");
-INSERT INTO cities (id, state_id, name) VALUES (11, 2, "Phoenix");
+-- Script to list cities of California without using JOIN
+USE hbtn_0d_usa;
 
 SELECT name
 FROM cities
-WHERE state_id = (SELECT state_id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
