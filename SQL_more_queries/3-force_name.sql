d03638f1e1d940493e671da9aa75ac4ab982a110
mesoud5 <mesoudtemam5@gmail.com>
2024-01-18 04:32:12 -0700

    TASK 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,8 @@
 -- CREATING TABLE
 USE hbtn_0d_2;
-CREATE TABLE IF NOT EXISTS force_name(
+
+-- If the 'force_name' table doesn't exist, create it
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
