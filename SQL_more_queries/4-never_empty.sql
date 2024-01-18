1f9152faa7a5b29631ac7b2c8708d5e9a07331be
mesoud5 <mesoudtemam5@gmail.com>
2024-01-18 04:43:21 -0700

    TASK 4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,1 +1,8 @@
--- CREATING NEW TABLE
+-- New database hbtn_test_db_4
+DROP DATABASE IF EXISTS hbtn_test_db_4;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
+-- CREATING THE TABLE id_not_null
+CREATE TABLE IF NOT EXISTS id_not_null ( 
+    id INT DEFAULT 1, 
+    name VARCHAR(256) 
+);
