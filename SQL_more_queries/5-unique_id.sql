0814a061a3ecc7dae5c3eeab02e72447ffdf0906
mesoud5 <mesoudtemam5@gmail.com>
2024-01-18 04:56:43 -0700

    TASK 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,9 @@
 -- New database hbtn_test_db_5
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+-- CREATING TABLE:unique_id
+CREATE TABLE IF NOT EXISTS unique_id ( 
+    id INT NOT NULL DEFAULT 1 PRIMARY KEY, 
+    name VARCHAR(256) 
+);
