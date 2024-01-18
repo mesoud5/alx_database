5683b6c0d9110a9f7b67277152ce28de4d2850b0
mesoud5 <mesoudtemam5@gmail.com>
2024-01-18 04:36:59 -0700

    TASK 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,8 @@
 -- CREATING TABLE
-USE hbtn_0d_2;
+-- New database hbtn_test_db_3
+DROP DATABASE IF EXISTS hbtn_test_db_3;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
+USE hbtn_test_db_3;
 
 -- If the 'force_name' table doesn't exist, create it
 CREATE TABLE IF NOT EXISTS force_name (
